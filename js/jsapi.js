8e547e26a740f2a35ec7bf454213c0a839337ef7
joosthoeks <joosthoeks@gmail.com>
2017-04-20 11:18:01 +0000

    new MADspace site

---
+++ js/jsapi.js
@@ -0,0 +1,8 @@
+//google.load('jquery','1');
+//google.load('jqueryui','1');
+//google.load('prototype','1');
+//google.load('scriptaculous','1');
+//google.load('mootools','1');
+//google.load('dojo','1');
+//google.load('swfobject','2');
+//google.load('yui','2');
